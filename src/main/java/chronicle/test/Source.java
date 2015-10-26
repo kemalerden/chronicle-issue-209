726a82dd2990ef69f5d6b704c818930db4de582d
Kemal Erden <kemal.erden@gmail.com>
2015-10-26 09:55:02 +0000

    Update Source.java

--- src/main/java/chronicle/test/Source.java
+++ src/main/java/chronicle/test/Source.java
@@ -7,21 +7,6 @@ import net.openhft.chronicle.Chronicle;
 import net.openhft.chronicle.ChronicleQueueBuilder;
 import net.openhft.chronicle.ExcerptAppender;
 
-/*
- * COPYRIGHT. HSBC HOLDINGS PLC 2015. ALL RIGHTS RESERVED.
- * 
- * This software is only to be used for the purpose for which it has been
- * provided. No part of it is to be reproduced, disassembled, transmitted,
- * stored in a retrieval system nor translated in any human or computer
- * language in any way or for any other purposes whatsoever without the prior
- * written consent of HSBC Holdings plc.
- */
-
-/**
- * <p>
- * <b> TODO : Insert description of the class's responsibility/role. </b>
- * </p>
- */
 public class Source {
 
     private final String sourcePath = "source";
@@ -61,7 +46,7 @@ public class Source {
             message.setControlEventId("0x000000000000000000000000000000000000001");
             message.setTimestamp(System.currentTimeMillis());
             message.setVersion("v1");
-            String str = str(10);
+            String str = str(10000);
             message.setPayload(str);
             try (ExcerptAppender appender = this.source.createAppender()) {
                 appender.startExcerpt(message.length());
